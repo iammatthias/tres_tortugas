1b9102126ab8662e168f96bd92086cc2f9617740
Matthias Jordan <matthias.k.jordan@gmail.com>
2020-07-02 21:29:53 +0000

    init

---
+++ src/components/layout.js
@@ -0,0 +1,36 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import { GlobalStyle } from "./globalStyle"
+import { MainWrapper } from "./layoutComponents"
+
+
+const Layout = ({ children }) => (
+    <StaticQuery
+        query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+        render={data => (
+            <>
+                <GlobalStyle />
+                <MainWrapper>
+
+                    <main>{children}</main>
+
+                </MainWrapper>
+            </>
+        )}
+    />
+)
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import { Container } from "../components/layoutComponents"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+    <Layout>
+        <Container>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
+            <div style={{ width: '100%', maxWidth: "100vw", height: '100vh', }}>
+                <img alt="turtles" style={{ width: '100%', maxWidth: "100vw", height: '100vh', objectFit: 'cover', margin: '0' }} src="https://source.unsplash.com/featured/2000?turtle" />
+            </div>
+
+        </Container>
+    </Layout>
+)
+
+export default IndexPage
